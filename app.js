4b831caae1c5cb780ffd30313410c066ada68711
Abhiram <abhikollepara333@gmail.com>
2025-03-18 11:09:24 +0530

    complected

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require("express")
+// const ErrorMiddleware=require("./middleware/error")
+const userRouter=require('./controllers/userRoutes')
+const app=express()
+app.use(express.json())
+
+
+app.use("/user",userRouter)
+
+
+
+
+
+
+
+
+
+
+module.exports = { app };
---
+++ model/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const DocterModel = require("../model/docterModel");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  doctorDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+  },
+  dateJoined: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+module.exports = UserModel;
