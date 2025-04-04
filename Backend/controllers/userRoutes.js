d938d324bb5f545eee8454b1b56de637b1c69f85
Abhiram <abhikollepara333@gmail.com>
2025-04-04 11:10:18 +0530

    Done

--- Backend/controllers/userRoutes.js
+++ Backend/controllers/userRoutes.js
@@ -170,13 +170,6 @@ userRouter.post("/login",catchAsyncError(async (req, res, next) => {
     });
   }));
 
-
-  userRouter.post("/logout",catchAsyncError(req,res,next)=>{
-    res.cleartoken()
-  })
-
-
-
 module.exports = userRouter;
 
 
