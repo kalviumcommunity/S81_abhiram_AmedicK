5e367a1440efeee65c19e83268dc059f11eb738c
Abhiram <abhikollepara333@gmail.com>
2025-05-05 16:51:27 +0530

    Done

--- Backend/controllers/docterRoutes.js
+++
@@ -1,4 +0,0 @@
-const {App} = require('../app')
-const catchAsyncError = require('../middleware/catchAsyncError')
-const ErrorHandler=require('../utils/errorhadler')
-const jwt = require("jsonwebtoken");
