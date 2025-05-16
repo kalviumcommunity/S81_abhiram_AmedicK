ed4874ee5219efaaf96188f882e2ba167ae38f2f
Abhiram <abhikollepara333@gmail.com>
2025-05-16 11:49:26 +0530

    Upload

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,22 +1,22 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+    const jwt = require("jsonwebtoken");
+    require("dotenv").config();
 
-const auth = (req, res, next) => {
-  const token = req.cookies.accesstoken;
-  // console.log(accesstoken,"12345678999")
-  if (!token) {
-    return res.status(401).json({ message: "Token not found" });
-  }
-
-  jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "Invalid or expired token" });
+    const auth = (req, res, next) => {
+    const token = req.cookies.accesstoken;
+    // console.log(accesstoken,"12345678999")
+    if (!token) {
+        return res.status(401).json({ message: "Token not found" });
     }
 
-    req.user_id = decoded.id;
-    req.user_role = decoded.role;
-    next();
-  });
-};
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+        return res.status(401).json({ message: "Invalid or expired token" });
+        }
 
-module.exports = { auth };
+        req.user_id = decoded.id;
+        req.user_role = decoded.role;
+        next();
+    });
+    };
+
+    module.exports = { auth };
